d8ee4a0adb6bfdaca50d7d589a1321bcade818ef
Nauman Baig <naumanmbaig@gmail.com>
2024-01-12 07:00:43 +0000

    Sync LeetCode submission - Merge Sorted Array (python)

--- my-folder/problems/merge_sorted_array/solution.py
+++ my-folder/problems/merge_sorted_array/solution.py
@@ -7,6 +7,21 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        for i in range(1,n+1):
-            nums1[m+n-i] = nums2[n-i]
-        nums1.sort()
+        # Indices tracker for the arrays
+        j = m-1
+        k = n-1
+        # starting from the end of the nums1 array
+        for i in range(m+n-1,-1,-1):
+            if k<0: # if nums2 is empty or has been consumed, leave nums1 as is
+                continue
+            elif j<0: # if num1 is empty or has been consumed, fill rest of nums1 with nums 2
+                nums1[i] = nums2[k]
+                k -= 1 # decrement nums2 index
+            else: # if both nums1 and nums2 are not empty
+                if nums1[j] >= nums2[k]: 
+                    nums1[i] = nums1[j] 
+                    j -= 1
+                else: 
+                    nums1[i] = nums2[k]
+                    k -= 1
+            
