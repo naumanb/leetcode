0610c4a2d08eb11059d07892488a387a298db12a
Nauman Baig <naumanmbaig@gmail.com>
2024-01-28 21:06:58 +0000

    Sync LeetCode submission - Roman to Integer (python)

--- my-folder/problems/roman_to_integer/solution.py
+++ my-folder/problems/roman_to_integer/solution.py
@@ -7,9 +7,14 @@ class Solution(object):
         roman_dict = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
         total = 0
 
-        for i in range(len(s)):
-            number = roman_dict[s[i]]
-            total += number
-            if i>0 and number > roman_dict[s[i-1]]:
-                total -= 2*roman_dict[s[i-1]]
+        s = s.replace("IV","IIII")
+        s = s.replace("IX","VIIII")
+        s = s.replace("XL","XXXX")
+        s = s.replace("XC","LXXXX")
+        s = s.replace("CD","CCCC")
+        s = s.replace("CM","DCCCC")
+
+
+        for char in s:
+            total += roman_dict[char]
         return total
