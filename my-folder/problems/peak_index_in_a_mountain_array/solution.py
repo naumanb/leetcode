0340e20da6e9c2bbc52a00f560d4e81d9d22744d
Nauman Baig <naumanmbaig@gmail.com>
2024-01-29 20:01:46 +0000

    Sync LeetCode submission - Peak Index in a Mountain Array (python)

--- my-folder/problems/peak_index_in_a_mountain_array/solution.py
+++ my-folder/problems/peak_index_in_a_mountain_array/solution.py
@@ -6,10 +6,11 @@ class Solution(object):
         """
         left = 0
         right = len(arr)-1
-        while left < right: # While the left index is less than the right index
-            mid = (left+right)//2 # Calculate the mid index
-            if arr[mid] < arr[mid+1]: # if the value at the mid index is less than the value at the next index
-                left = mid+1 # set the left index to the next index
+
+        while left < right:
+            mid = (left + right)//2
+            if arr[mid] < arr[mid+1]:
+                left = mid+1
             else:
-                right = mid # set the right index to the mid index
-        return left # return the left pointer as the peak index
+                right = mid
+        return left
