642b22f561633501bb45a1022e8cb0acbfdbe1c9
Nauman Baig <naumanmbaig@gmail.com>
2024-02-15 20:52:29 +0000

    Sync LeetCode submission - Sort List (python)

---
+++ my-folder/problems/sort_list/solution.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+
+        ptr = head
+        vals = []
+
+        while ptr:
+            vals.append(ptr.val)
+            ptr = ptr.next
+
+        vals.sort()
+
+        ptr = head
+        for val in vals:
+            ptr.val = val
+            ptr = ptr.next
+
+        return head
